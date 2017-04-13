175c5ace43549f408d8939b2755db716eca8b6e5
TodMcChesney <codescribe.net@gmail.com>
2017-04-13 10:20:44 +0200

    refactor: Use .text() instead of .html()
    Simplify both the html and js by using .text() to update the counter.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,5 @@ var numClicks = 0;
 
 $('.main-image').click(function() {
     numClicks += 1;
-    var updatedCounter = '<p class="clicks">Number of Clicks: ' + numClicks + '</p>';
-    $('.counter').html(updatedCounter);
+    $('.counter').text('Number of Clicks: ' + numClicks);
 });
